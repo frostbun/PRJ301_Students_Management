3994563965177f945c06c967d2b486053e6df165
thanhienee <97795655+thanhienee@users.noreply.github.com>
2022-06-01 23:32:44 +0700

    Update Student_Management.sql
    edit database formatting

--- Student_Management.sql
+++ Student_Management.sql
@@ -1,60 +1,58 @@
-create database StudentManagement
 
 create table [User](
-	RollNumber nvarchar(10) not null,
-	FirstName nvarchar(50),
-	LastName nvarchar(50),
-	PhoneNumber nvarchar(10),
-	Email nvarchar(50),
-	Address nvarchar(100),
-	UserName nvarchar(50),
-	Password nvarchar(50) not null,
-	primary key(Username)
+	rollNumber nvarchar(10) not null,
+	firstName nvarchar(50),
+	lastName nvarchar(50),
+	phoneNumber nvarchar(10),
+	email nvarchar(50),
+	address nvarchar(100),
+	username nvarchar(50),
+	password nvarchar(50) not null,
+	primary key(username)
 )
 
 create table Class(
-   ClassID nvarchar(10),
-   Name nvarchar(20),
-   InviteCode nvarchar(20) unique,
+   classID nvarchar(10),
+   name nvarchar(20),
+   inviteCode nvarchar(20) unique,
    primary key(ClassID)
 )
 
 create table ClassMember(
-	Username nvarchar(50) foreign key references [User](Username),
-	ClassID nvarchar(10) foreign key references Class(ClassID),
-	Role nvarchar(7) not null, 
-	check(Role in ('Student', 'Teacher'))
+	username nvarchar(50) foreign key references [User](username),
+	classID nvarchar(10) foreign key references Class(classID),
+	role nvarchar(7) not null, 
+	check(role in ('Student', 'Teacher'))
 )
 
 create table Homework(
-	HomeworkID nvarchar(10) primary key,
-	FileLink nvarchar,
-	Discription nvarchar,
-	ClassID nvarchar(10) foreign key references Class(ClassID),
-	Username nvarchar(50) foreign key references [User](Username),
-	Deadline Date,
+	homeworkID nvarchar(10) primary key,
+	fileLink nvarchar,
+	discription nvarchar,
+	classID nvarchar(10) foreign key references Class(classID),
+	username nvarchar(50) foreign key references [User](username),
+	deadline Date,
 )
 
 create table Submission(
-	HomeworkID nvarchar(10),
-	FileLink nvarchar,
-	Username nvarchar(50) foreign key references [User](Username),
-	Mark int,
-	primary key(HomeworkID, Username)
+	homeworkID nvarchar(10),
+	fileLink nvarchar,
+	username nvarchar(50) foreign key references [User](username),
+	mark int,
+	primary key(homeworkID, username)
 )
 
 create table MailRequest(
-	Username nvarchar(50) foreign key references [User](Username),
-	Title nvarchar,
-	Content nvarchar,
-	ClassID nvarchar(10) foreign key references Class(ClassID)
+	username nvarchar(50) foreign key references [User](username),
+	title nvarchar,
+	content nvarchar,
+	classID nvarchar(10) foreign key references Class(classID)
 )
 
 create table MailResponse(
-	Username nvarchar(50) foreign key references [User](Username),
-	Title nvarchar,
-	Content nvarchar,
-	ClassID nvarchar(10) foreign key references Class(ClassID)
+	username nvarchar(50) foreign key references [User](username),
+	title nvarchar,
+	content nvarchar,
+	classID nvarchar(10) foreign key references Class(classID)
 )
 
-drop database StudentManagement
