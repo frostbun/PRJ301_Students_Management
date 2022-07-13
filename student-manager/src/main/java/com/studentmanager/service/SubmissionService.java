f20d8ac856acb782264ad9da4c0249974b2af184
thanhienee <hienntthe160135@fpt.edu.vn>
2022-07-13 15:12:38 +0700

    commit

--- student-manager/src/main/java/com/studentmanager/service/SubmissionService.java
+++ student-manager/src/main/java/com/studentmanager/service/SubmissionService.java
@@ -22,7 +22,7 @@ public class SubmissionService {
     @Autowired
     private SubmissionRepository submissionRepo;
 
-    public ServiceResponse<Submission> mark(Submission submission, Double mark) {
+    public ServiceResponse<Submission> mark(Submission submission, Double mark                              ) {
         if (mark == null || mark < 0 || (submission.getHomework().getMaxMark() != null && mark > submission.getHomework().getMaxMark())) {
             return ServiceResponse.error("Invalid mark");
         }
